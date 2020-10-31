2fede120d80d22745f14b48b30a10869cf4370b1
Mark Washkewicz <mwashkewicz@gmail.com>
2020-10-31 10:21:42 -0700

    added index html to server so can load webpage

--- server/server.js
+++ server/server.js
@@ -18,9 +18,30 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use('/build', express.static(path.join(__dirname, '../build')));
 
 //all calls to website/api go to the router
-app.use('/api', apiRouter);
+// app.use('/api', apiRouter);
 
+//send index html to link to our react app
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'));
+    res.sendFile(path.join(__dirname, '../client/index.html'));
   });
 
+  
+
+//internal errors
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
+
+module.exports = app;
