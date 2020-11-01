d46e7f4d5ed8b56c708603aa9ed03cee6be5e1f7
Elise Bare <47790501+elisebare@users.noreply.github.com>
2020-11-01 10:38:12 -0800

    (add) the week component currently displays an h2 tag and the start date of that week

--- client/component/week.jsx
+++ client/component/week.jsx
@@ -5,9 +5,13 @@ import { render } from "react-dom";
 class Week extends Component {
  
   render() {
+    const firstSunday = new Date(this.props.year, this.props.month, this.props.firstSundayDate)
 
     return (
-
+      <div className = "calendarRow">
+        <h2>This is a week</h2>
+        <h3>{firstSunday.toDateString()}</h3>
+      </div>
       
     );
   }
