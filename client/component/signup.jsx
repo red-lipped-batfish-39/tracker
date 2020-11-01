ec45ed199e3f2c2ec9ff64dd4b5170a70294466e
Elise Bare <47790501+elisebare@users.noreply.github.com>
2020-11-01 08:41:21 -0800

    (update) added line breaks and class names for buttons

--- client/component/signup.jsx
+++ client/component/signup.jsx
@@ -11,19 +11,21 @@ class Signup extends Component {
       //user name, pass, button
       <div className={"loginContainer"}>
 
-      <h1>This is Signup</h1>
+        <h1>Create an Account</h1>
 
-      <label htmlFor="userID"> Input username</label>
-      <input value = {this.props.username} type="text" id="userID" name="userID"  onChange = {(e) => {this.props.trackInput('username', e)}}/>
+        <label htmlFor="userIDSignup"> Input username</label>
+        <input value = {this.props.username} type="text" id="userIDSignup" name="userIDSignup"  onChange = {(e) => {this.props.trackInput('username', e)}}/>
 
-      <label htmlFor="pwd"> password</label>
-      <input value = {this.props.password} type="password" id="pwd" name="pwd" onChange = {(e) => {this.props.trackInput('password', e)}}/>
-      
-      <label htmlFor="email"> email</label>
-      <input value = {this.props.email} type="text" id="email" name="email" onChange = {(e) => {this.props.trackInput('email', e)}}/>
+        <label htmlFor="pwdSignUp"> password</label>
+        <input value = {this.props.password} type="password" id="pwdSignUp" name="pwdSignUp" onChange = {(e) => {this.props.trackInput('password', e)}}/>
+        
+        <label htmlFor="email"> email</label>
+        <input value = {this.props.email} type="text" id="email" name="email" onChange = {(e) => {this.props.trackInput('email', e)}}/><br></br>
 
 
-      <button onClick = {this.props.signup}>Create user </button>
+        <button className="primaryButton" onClick = {this.props.signup}>Create account </button><br></br>
+
+        <button className="secondaryButton" onClick = {this.props.changeTask}>Go to log in page</button><br></br>
   
       </div>
     );
