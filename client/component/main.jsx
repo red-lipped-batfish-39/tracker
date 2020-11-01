7eef5d91266c597bd1bd8894f9ce3fd236a369c0
Elise Bare <47790501+elisebare@users.noreply.github.com>
2020-11-01 08:54:57 -0800

    (update) working on flex containers for new period inputs

--- client/component/main.jsx
+++ client/component/main.jsx
@@ -8,18 +8,18 @@ class Main extends Component {
 
     return (
 
-      <div>
-        <h1>This is Main</h1>
+      <div class="periodInputContainer">
+        <h1>Add new period</h1>
         <label htmlFor="start"> Start Date</label>
         <input type = "date" id = "startDate" name = "start" 
-        onChange = {(e) => {this.props.trackInput('startDate', e)}}
-        />
+          onChange = {(e) => {this.props.trackInput('startDate', e)}}
+        /><br></br>
 
         <label htmlFor="end">End Date</label>
         <input type = "date" id = "endDate" name = "end"
-        onChange = {(e) => {this.props.trackInput('endDate', e)}}
-        />
-        <button onClick = {this.props.newPeriod}>submit new period</button>
+          onChange = {(e) => {this.props.trackInput('endDate', e)}}
+        /><br></br>
+        <button className ="primaryButton" onClick = {this.props.newPeriod}>Submit these dates</button><br></br>
       </div>
     );
   }
