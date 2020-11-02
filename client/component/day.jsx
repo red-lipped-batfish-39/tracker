f2d5f0884f61d0f61f1a3d60278994e7384e9b2b
Elise Bare <47790501+elisebare@users.noreply.github.com>
2020-11-01 20:13:59 -0800

    (update) added click handler to dDay components to setPeriodDates in state, added highlight class to show range of dates selected by user before submitting dates to db

--- client/component/day.jsx
+++ client/component/day.jsx
@@ -3,12 +3,35 @@ import { render } from "react-dom";
 
 
 class Day extends Component {
- 
+  
   render() {
+    //using the year month and date passed down, make a date object (today)
     const today = new Date(this.props.year, this.props.month, this.props.date);
+
+    //convert date object to an ISO string and slice only YYYY-MM-DD
+    let dateString = today.toISOString().slice(0, 10);
+    
+    //if the dateString is in the period List OR is the current start or end date -- it should be highlighted. 
+    //we will accomplish that with a different className
+    let className = '';
+    if (dateString === this.props.startDate || dateString === this.props.endDate) {
+      className = 'highlight'
+    } else if (dateString > this.props.startDate && dateString < this.props.endDate) {
+      className = 'highlight'
+    }
     return (
-      <div className="dayBox" key={today.toDateString()} id={today.toDateString()}>
-        <p>{this.props.date}</p>
+      <div
+        className="dayBox" 
+      >
+        <p
+          key={dateString} 
+          id={dateString}
+          //add class for bg color
+          className = {className}
+          onClick = {this.props.setPeriodDates}
+        >
+          {this.props.date}
+        </p>
       </div>
       
     );
